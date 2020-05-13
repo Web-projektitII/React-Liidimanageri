4b7df2950eb76d92b40b958ab7a6a04369616b71
60734740 <jaula@omnia.fi>
2020-05-13 17:50:44 +0300

    Sovelluksen nimi

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header(){
     return (
         <header className="border-b p-3 flex justify-between items-center">
             <Link to="/" className="font-bold">
-                AppName
+                React-sovellusmalli
             </Link>
 
             <Navigation />
